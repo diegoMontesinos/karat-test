96c83e4050f0274e86d98298f4e2e0ae45a3d9cc
Diego Montesinos <chesp.gommont@gmail.com>
2024-03-29 21:17:30 -0600

    Test transactions and authorizations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,40 @@
 import express, { Request, Response } from 'express';
-// import Stripe from 'stripe';
+import Stripe from 'stripe';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
 dotenv.config();
 
-// const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-// const STRIPE_CARD_ID = process.env.STRIPE_CARD_ID;
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || '';
+const STRIPE_CARD_ID = process.env.STRIPE_CARD_ID;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-// const stripe = new Stripe(STRIPE_SECRET_KEY);
+const stripe = new Stripe(STRIPE_SECRET_KEY);
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/api/transactions', async (_: Request, res: Response) => {
-  console.log(123);
-  // try {
-  //   const transactions = await stripe.issuing.transactions.list({
-  //     card: 'your_card_id',
-  //   });
-  //   res.json(transactions);
-  // } catch (error) {
-  //   if (error instanceof Error) {
-  //     res.status(500).json({ error: error.message });
-  //   } else {
-  //     res.status(500).json({ error: 'An unexpected error occurred' });
-  //   }
-  // }
+  try {
+    await stripe.issuing.authorizations.list({
+      card: STRIPE_CARD_ID,
+      limit: 3,
+    });
+
+    const transactions = await stripe.issuing.transactions.list({
+      card: STRIPE_CARD_ID,
+    });
+
+    console.log(transactions.has_more, transactions.data.length);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    }
+  }
+
   res.json({ test: 1123 });
 });
 
